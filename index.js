17cfacaedbfcadca00b7984e8d577d2f06029bf2
MarioJr92 <mariorodjr92@gmail.com>
2022-06-15 22:55:15 -0400

    added function to delete tasks

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+$(document).ready(function () {
   var getAndDisplayAllTasks = function () {
     $.ajax({
       type: 'GET',
@@ -7,7 +7,7 @@ $(document).ready(function(){
       success: function (response, textStatus) {
         $('#todo-list').empty();
         response.tasks.forEach(function (task) {
-          $('#todo-list').append('<p>' + task.content + '</p>');
+          $('#todo-list').append('<div class="row"><p class="col-xs-8">' + task.content + '</p><button class="delete" data-id="' + task.id + '">Delete</button>');
         })
       },
       error: function (request, textStatus, errorMessage) {
@@ -15,8 +15,8 @@ $(document).ready(function(){
       }
     });
   }
-  
-  var createTask = function () {
+
+  var createTask = () => {
     $.ajax({
       type: 'POST',
       url: 'https://altcademy-to-do-list-api.herokuapp.com/tasks?api_key=470',
@@ -34,14 +34,27 @@ $(document).ready(function(){
       error: function (request, textStatus, errorMessage) {
         console.log(errorMessage);
       }
-    });  
+    });
   }
-  
+
   $('#create-task').on('submit', function (e) {
     e.preventDefault();
     createTask();
   });
-  
+
   getAndDisplayAllTasks();
-  
+
+  var deleteTask = id => {
+    $.ajax({
+      type: 'DELETE',
+      url: `https://altcademy-to-do-list-api.herokuapp.com/tasks/${id}?api_key=470`,
+      success: function (response, textStatus) {
+        getAndDisplayAllTasks();
+      },
+      error: function (request, textStatus, errorMessage) {
+        console.log(errorMessage);
+      }
+    });
+  }
+
 });
