97a0af3f008f3f3ae3b99d3cc4d42c7637e0e353
MarioJr92 <mariorodjr92@gmail.com>
2022-06-15 23:05:47 -0400

    added function to mark task complete

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ $(document).ready(function () {
       success: function (response, textStatus) {
         $('#todo-list').empty();
         response.tasks.forEach(function (task) {
-          $('#todo-list').append('<div class="row"><p class="col-xs-8">' + task.content + '</p><button class="delete" data-id="' + task.id + '">Delete</button>');
+          $('#todo-list').append(`<div class="row"><p class="col-xs-8">${task.content}</p><button class="delete" data-id="${task.id}">Delete</button><input type="checkbox" class="mark-complete" data-id="${task.id}"${task.completed ? 'checked' : ''}>`);
         })
       },
       error: function (request, textStatus, errorMessage) {
@@ -48,6 +48,24 @@ $(document).ready(function () {
     $.ajax({
       type: 'DELETE',
       url: `https://altcademy-to-do-list-api.herokuapp.com/tasks/${id}?api_key=470`,
+      success: function (response, textStatus) {
+        getAndDisplayAllTasks();
+      },
+      error: function (request, textStatus, errorMessage) {
+        console.log(errorMessage);
+      }
+    });
+  }
+
+  $(document).on('click', '.delete', function () {
+    deleteTask($(this).data('id'));
+  });
+
+  var markTaskComplete = function (id) {
+    $.ajax({
+      type: 'PUT',
+      url: `https://altcademy-to-do-list-api.herokuapp.com/tasks/${id}/mark_complete?api_key=48`,
+      dataType: 'json',
       success: function (response, textStatus) {
         getAndDisplayAllTasks();
       },
